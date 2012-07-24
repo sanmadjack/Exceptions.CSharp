44a8f5692288518fb5881d61715e4c8325059b29
Matthew Barbour <sanmadjack@gmail.com>
2012-07-24 16:50:15 -0500

    Updated version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Exceptions")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Kohls Department Stores Inc")]
+[assembly: AssemblyCompany("Matthew Barbour")]
 [assembly: AssemblyProduct("Exceptions")]
-[assembly: AssemblyCopyright("Copyright © Kohls Department Stores Inc 2012")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
