9d19a3c8b1ef21e8cbe62478181634cc73800164
Matthew Barbour <sanmadjack@gmail.com>
2012-08-14 14:13:06 -0500

    Added WTFException

---
+++ WTFException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Exceptions {
+    public class WTFException: Exception {
+        public WTFException(Exception ex) : base(null,ex) { }
+        public WTFException(string message, Exception ex) : base(message, ex) { }
+        public WTFException(string message) : base(message) { }
+    }
+}
