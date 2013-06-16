8270040eb693b0de31a77f3a064eeb0bf24b0ee6
Matthew Barbour <sanmadjack@gmail.com>
2013-06-16 13:30:03 -0500

    Added DirectoryNotFoundException

---
+++ DirectoryNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Exceptions {
+	public class DirectoryNotFoundException: Exception {
+		public DirectoryNotFoundException(string message): base(message) {
+		}
+	}
+}
